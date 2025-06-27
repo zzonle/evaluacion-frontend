0eee4140708b5a8217111ec271d35774340d98b7
zzonle <bastianofedamaldonado@gmail.com>
2025-06-27 18:34:19 -0400

    configuración tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            primary: '#1f2937', // Gris oscuro (formal)
+            accent: '#3b82f6',  // Azul serio
+            soft: '#f3f4f6'     // Fondo suave
+          }
+        }
+      }
+    }
